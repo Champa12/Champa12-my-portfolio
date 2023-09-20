937f15d222c3c32573ac8a3f3a8caa09c5c8087b
Champa <hossainchampa23@gmail.com>
2023-09-20 17:21:42 -0400

    modified

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import emailjs from '@emailjs/browser';
+import emailjs from "@emailjs/browser";
 import { useForm } from "react-hook-form";
 
 const Contact = () => {
